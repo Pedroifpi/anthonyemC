2c231b8cc55412c8518f788454a7c632757464e1
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2026-05-28 16:25:17 -0300

    Atualizar o unsigned.c

--- unsigned.c
+++ unsigned.c
@@ -1,20 +1,23 @@
-
 #include <stdio.h>
 
 int main() {
 
-    // Variável para guardar o número
-    unsigned int num;
+    int num;
 
-    // Pede o número ao usuário
-    printf("Digite um numero: ");
-    scanf("%u", &num);
+    printf("Digite um numero positivo: ");
+    scanf("%d", &num);
 
-    // Percorre os bits do 7 até o 0
+    // Verifica se o número é negativo
+    if(num < 0) {
+
+        printf("Erro: numero negativo nao permitido.");
+
+        return 0;
+    }
+
+    // Mostra o número em binário
     for(int i = 7; i >= 0; i--) {
 
-        // Move os bits para direita
-        // e pega somente o último bit
         printf("%d", (num >> i) & 1);
     }
 
