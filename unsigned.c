16085045d119c9e1dfaa94a347fa53e8840dad36
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2026-05-28 14:13:25 -0300

    Atualizar o unsigned.c

--- unsigned.c
+++ unsigned.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main() {
+
+    // Variável para guardar o número
+    unsigned int num;
+
+    // Pede o número ao usuário
+    printf("Digite um numero: ");
+    scanf("%u", &num);
+
+    // Percorre os bits do 7 até o 0
+    for(int i = 7; i >= 0; i--) {
+
+        // Move os bits para direita
+        // e pega somente o último bit
+        printf("%d", (num >> i) & 1);
+    }
+
+    return 0;
+}
