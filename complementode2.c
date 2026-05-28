71a575330fe3296fef503ac24e71abf501771921
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2026-05-28 14:14:10 -0300

    Atualizar o complementode2.c

--- complementode2.c
+++ complementode2.c
@@ -1,1 +1,22 @@
 
+#include <stdio.h>
+
+int main() {
+
+    // Variável do número
+    int num;
+
+    // Entrada do usuário
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    // Percorre os 8 bits
+    for(int i = 7; i >= 0; i--) {
+
+        // Move os bits para direita
+        // e pega apenas o último bit
+        printf("%d", (num >> i) & 1);
+    }
+
+    return 0;
+}
