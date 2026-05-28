6fdf684692027f00305b2e689b859888ad3600fc
pedro lucas <160865525+Pedroifpi@users.noreply.github.com>
2026-05-28 14:13:49 -0300

    Atualizar o sinalemagnitude.c

--- sinalemagnitude.c
+++ sinalemagnitude.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main() {
+
+    // Variável do número
+    int num;
+
+    // Entrada do usuário
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    // Verifica se é negativo
+    if(num < 0) {
+
+        // Mostra bit de sinal 1
+        printf("1");
+
+        // Transforma em positivo
+        num = -num;
+    }
+    else {
+
+        // Se for positivo
+        // imprime sinal 0
+        printf("0");
+    }
+
+    // Mostra os outros 7 bits
+    for(int i = 6; i >= 0; i--) {
+
+        // Imprime bit por bit
+        printf("%d", (num >> i) & 1);
+    }
+
+    return 0;
+}
